86dd368b76458b0ddb2a0ea8a0a72b0ec9ed722e
Martin Treml <martin@treml.dev>
2024-12-29 01:03:07 +0100

    fix listening in fterm terminal for lazygit

--- modules/home-manager/nvim/config/lua/plugins/fterm.lua
+++ modules/home-manager/nvim/config/lua/plugins/fterm.lua
@@ -4,7 +4,6 @@ return {
 		local fterm = require("FTerm")
 
 		vim.keymap.set("n", "<leader>tt", function() fterm:toggle() end, {desc = "[T]oggle [T]erminal"})
-		vim.keymap.set("t", "<leader>tt", function() fterm:toggle() end, {desc = "[T]oggle [T]erminal"})
 
 		local lazygit = fterm:new({
 			ft = 'fterm_lazygit',
@@ -12,6 +11,5 @@ return {
 		})
 
 		vim.keymap.set("n", "<leader>lg", function() lazygit:toggle() end, {desc = 'Toggle [L]azy[G]it'})
-		vim.keymap.set("t", "<leader>lg", function() lazygit:toggle() end, {desc = 'Toggle [L]azy[G]it'})
 	end
 }

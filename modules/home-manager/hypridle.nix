b026d15b4154966ff6ce603b9bafaa0074f1f92b
Martin Treml <git@treml.dev>
2025-02-27 16:41:16 +0100

    Set hypridle timeouts

--- modules/home-manager/hypridle.nix
+++ modules/home-manager/hypridle.nix
@@ -19,6 +19,27 @@ in
           before_sleep_cmd = "loginctl lock-session";
           after_sleep_cmd = "hyprctl dispatch dpms on";
         };
+
+				listener = [
+					{
+				    timeout = 150;                                # 2.5min.
+				    on-timeout = "brightnessctl -s set 10";         # set monitor backlight to minimum, avoid 0 on OLED monitor.
+				    on-resume = "brightnessctl -r";                 # monitor backlight restor.
+					}
+					{
+						timeout = 900;                                 # 15min
+						on-timeout = "loginctl lock-session";            # lock screen when timeout has passed
+					}
+					{
+						timeout = 1200;                                 # 20min
+						on-timeout = "hyprctl dispatch dpms off";        # screen off when timeout has passed
+						on-resume = "hyprctl dispatch dpms on";          # screen on when activity is detected after timeout has fired.
+					}
+					{
+						timeout = 1800;                                # 30min
+						on-timeout = "systemctl suspend";                # suspend pc
+					}
+				];
       };
     };
   };
